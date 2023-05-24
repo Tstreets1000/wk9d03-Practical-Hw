7d95c2d80031520a1e6427d0bd0a4403a884df4d
Tstreets1000 <terrance.jdhill@yahoo.com>
2023-05-23 22:54:53 -0400

    Week 9 Practical HW

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const fs = require('fs')
+const port = 3000
+
+const app = express()
+
+
+//======== Routes Start Here =============//
+app.get('/greeting/:name', (req, res) => {
+    const whatsUp = req.params.name
+
+    res.send(`Hello ${whatsUp}! It's so great to see you!`)
+})
+
+app.get('/greet/:name', (req, res) => {
+  const whatsUp = req.params.name
+
+  res.send(`Hi ${whatsUp}! It's so great to see you!`)
+})
+
+
+
+
+//==========Routes End Here =============//
+
+app.listen(3000, () => { 
+  console.log('Yes I am listening on port 3000')
+})
+
